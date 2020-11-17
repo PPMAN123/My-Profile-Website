9f7e075c14144b3e942098de720b2111be680737
Ethan Zhou <ethanzhou76@gmail.com>
2020-11-17 15:46:51 -0500

    Added scrolling detection with the top bar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const triggerFadeIn = () =>{
             hiddenElement.style.opacity = 1
             hiddenElement.style.transform='translateY(0px)'
         }, index*100);
-    })
+    });
 }
 
 const toggleMobileNav = (e) => {
@@ -37,6 +37,13 @@ const attatchOnSubmitToForm = () =>{
     })}
 }
 
+attachListenerToWindowUnload = () =>{
+    window.addEventListener('beforeunload' , e =>{
+        e.preventDefault();
+        e.returnValue = 'Benis';
+    })
+}
+
 window.onContactFormSubmit = token =>{
     console.log(token);
     const form = document.querySelector('form.contact-form');
@@ -45,8 +52,57 @@ window.onContactFormSubmit = token =>{
     }
 }
 
+const hideheaderArea = () =>{
+    const headArea = document.querySelector('.header-area');
+    headArea.classList.add('hide');
+}
+
+const showHeaderArea = () =>{
+    const headArea = document.querySelector('.header-area');
+    headArea.classList.remove('hide');
+}
+
+const attachListenerToScroll = () =>{
+    let lastScrollPosition = 0;
+    const scrollHandler = e =>{
+            console.log(e);
+            const currentScrollPosition = window.scrollY;
+            if(currentScrollPosition > lastScrollPosition){
+                hideheaderArea();
+            }else{
+                showHeaderArea();
+            }
+            lastScrollPosition = currentScrollPosition;
+    }
+    const throttledScrollHander = _.throttle(scrollHandler, 300);
+    window.addEventListener('scroll' , throttledScrollHander);
+}
+
+const requestForImage = () =>{
+    const requestPromise = axios.get('https://picsum.photos/200/300');
+    requestPromise.then(response =>{
+        const content = response.data;
+        console.log(content);
+    }).catch(error => {
+        console.error(error);
+    })
+}
+
+// const asyncMethod = async () =>{
+
+// }
+
 window.onload = function(){
     triggerFadeIn();
     attachMobileNavButton();
     attatchOnSubmitToForm();
+    attachListenerToScroll();
+    // attachListenerToWindowUnload();
+    // alert("boo");
 }
+
+//HTTP requests
+//1. GET
+//2. POST
+//3. DELETE
+//4. PATCH
